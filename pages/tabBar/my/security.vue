1fe1d67d3dd087644b6f28de34d42b8f25e36585
Dowling007 <1327716235@qq.com>
2020-05-29 14:20:59 +0800

    首页安全保障+ 安全保障详情接口渲染

--- pages/tabBar/my/security.vue
+++ pages/tabBar/my/security.vue
@@ -8,9 +8,22 @@
 		<view class="security">
 			<view class="securityimg">
 				<image src="../../../static/of/p4.jpg.png" mode=""></image>
+				<!-- <image :src="titleContent.Logo" mode=""></image> -->
 			</view>
-			<view class="security-box">
-				<view class="headline">房源真实可靠</view>
+			<view class="security-box" v-if="tabIndex===0">
+				<view class="headline" v-html="detailContent.OneTitle">房源真实可靠</view>
+				<view class="content" v-html="detailContent.OneContent">若发现房源严重不符合描述并影响入住，经核实与协调仍无法入住，我们将最高全额退款并补偿您相当于首晚房费的礼金券</view>
+			</view>
+			<view class="security-box" v-if="tabIndex===1">
+				<view class="headline" v-html="detailContent.TwoTitle">房源真实可靠</view>
+				<view class="content" v-html="detailContent.TwoContent">若发现房源严重不符合描述并影响入住，经核实与协调仍无法入住，我们将最高全额退款并补偿您相当于首晚房费的礼金券</view>
+			</view>
+			<view class="security-box" v-if="tabIndex===2">
+				<view class="headline" v-html="detailContent.ThreeTitle">房源真实可靠</view>
+				<view class="content" v-html="detailContent.ThreeContent">若发现房源严重不符合描述并影响入住，经核实与协调仍无法入住，我们将最高全额退款并补偿您相当于首晚房费的礼金券</view>
+			</view>
+			<!-- <view class="security-box">
+				<view class="headline" >房源真实可靠</view>
 				<view class="content">若发现房源严重不符合描述并影响入住，经核实与协调仍无法入住，我们将最高全额退款并补偿您相当于首晚房费的礼金券</view>
 			</view>
 			<view class="security-box">
@@ -20,18 +33,24 @@
 			<view class="security-box">
 				<view class="headline">费用透明安全</view>
 				<view class="content">若工作人员无故取消订单或提出不合理的站外收款，经核实与协调仍无法入住，我们将最高全额退款并补偿您相当于首晚房费的礼金券</view>
-			</view>
+			</view> -->
 		</view>
 	</view>
 </template>
 
 <script>
+	import {post} from '@/utils';
 	export default {
 		data() {
 			return {
 				tabList: ['放心入住', '专业客服', '安全无忧'],
 				tabIndex: 0,
+				titleContent: "",
+				detailContent: ""
 			}
+		},
+		onLoad() {
+			this.getSecurity()
 		},
 		computed: {
 			tabStyle() {
@@ -46,7 +65,18 @@
 				this.page = 1;
 				this.noDataIsShow = false;
 				this.hasData = false;
-				
+			},
+			async getSecurity(){
+				let titleContent = await post("/About/AboutUs",{Id:2,type:0})
+				let detailContent = await post("/About/AboutUs",{Id:2,type:1})
+				// 正则增加富文本的样式
+				detailContent.data.OneContent = detailContent.data.OneContent.replace(/<img/g, '<img style="max-width:100%;"');
+				detailContent.data.TwoContent = detailContent.data.TwoContent.replace(/<img/g, '<img style="max-width:100%;"');
+				detailContent.data.ThreeContent = detailContent.data.ThreeContent.replace(/<img/g, '<img style="max-width:100%;"');
+				this.titleContent = titleContent.data
+				this.detailContent = detailContent.data
+				console.log("安全保障titleRes：---",titleContent)
+				console.log("安全保障detailRes：---",detailContent)
 			},
 		},
 		
