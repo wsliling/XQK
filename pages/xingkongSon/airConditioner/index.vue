359d76f6929cd2cdf895a02cda92292108b9d084
Dowling007 <1327716235@qq.com>
2020-07-06 11:20:03 +0800

    星控空调开关模式风速的功能初步版本，星控灯光模式弹窗切换显示

--- pages/xingkongSon/airConditioner/index.vue
+++ pages/xingkongSon/airConditioner/index.vue
@@ -11,19 +11,25 @@
 							<image src="/static/xingkong/taiyang2.png" mode="widthFix"></image>
 						</view>
 						<view class="status">
-							<view class="model">
+							<view class="model" :class="[isAir ? 'canSee' : '']">
 								<view class="left">
-									制冷模式
+									<!-- 制冷模式 -->
+									{{ nowAir.model }}模式
 								</view>
 								<view class="center">
 									|
 								</view>
 								<view class="right">
-									自动风
+									<!-- 自动风 -->
+									{{ nowAir.wind }}
 								</view>
 							</view>
-							<view class="describe">
-								20°
+							<view v-if="isAir" class="describe">
+								<!-- 20° -->
+								{{ nowAir.wendu }}
+							</view>
+							<view v-else class="describe close-describe">
+								已关机
 							</view>
 						</view>
 					</view>
@@ -34,8 +40,8 @@
 			<!-- 控制内容盒子 -->
 			<view class="content-box">
 				<!-- 开关盒子 -->
-				<view class="switch-box flex-center-center">
-					<view class="switch flex-center-center">
+				<view @click="switchAir()" class="switch-box flex-center-center">
+					<view class="switch flex-center-center" :class="[!isAir ? 'close-bg' : '']">
 						<image src="/static/xingkong/switch0.png" mode="widthFix"></image>
 					</view>
 					<view class="switch-text">
@@ -44,10 +50,11 @@
 				</view>
 				<!-- 模式盒子 -->
 				<view class="model-box flex-center-between">
-					<view class="model-item" :class="[modelIndex === index ? 'active' : '',index === 0 ? 'w42' : '',index === 3 ? 'w39' : '']" v-for="(item,index) in modeList" :key='index'
-					 @click="changeModel(item,index)">
-						<image v-if="modelIndex !== index || modelIndex === -1" :src="item.img" mode="widthFix"></image>
-						<image v-else :src="item.activeImg" mode="widthFix"></image>
+					<view class="model-item" :class="[item.active ? 'active' : '',index === 0 ? 'w42' : '',index === 3 ? 'w39' : '']"
+					 v-for="(item,index) in modelList" :key='index' @click="changeModel(item,index)">
+						<!-- 	<image v-if="modelIndex !== index || modelIndex === -1" :src="item.img" mode="widthFix"></image>
+						<image v-else :src="item.activeImg" mode="widthFix"></image> -->
+						<image :src="item.active ? item.activeImg : item.img " mode=""></image>
 						<view class="model-value">
 							{{ item.value }}
 						</view>
@@ -62,7 +69,7 @@
 						<view class="left">
 							<view class="wind-speed-grade">
 								<view class="progress-bar">
-									<view class="active-progress-bar">
+									<view class="active-progress-bar"  :class="[isAir ? 'switch' : 'close-bg']">
 									</view>
 								</view>
 								<view class="wind-speed-describe">
@@ -72,18 +79,18 @@
 								</view>
 							</view>
 						</view>
-						<view class="right">
+						<view @click="changeAutoModel()" class="right" :class="[autoModel.active ? 'active' : '']">
 							<view class="huan flex-center-center">
 								<view class="huan-yuan">
-									
+
 								</view>
 							</view>
 							<view class="auto-model">
-								自动
+								{{ autoModel.value }}
 							</view>
 						</view>
 					</view>
-					
+
 
 				</view>
 			</view>
@@ -97,28 +104,41 @@
 		data() {
 			return {
 				bgImg: '/static/xingkong/bg1.png',
-				modelIndex: -1,
-				modeList: [{
+				isAir: false,
+				modelIndex: 0,
+				activeModel: {},
+				modelList: [{
 						img: '/static/xingkong/cool0.png',
 						activeImg: '/static/xingkong/cool1.png',
 						value: '制冷',
+						active: true,
 					},
 					{
 						img: '/static/xingkong/zhire0.png',
 						activeImg: '/static/xingkong/zhire1.png',
 						value: '制热',
+						active: false,
 					},
 					{
 						img: '/static/xingkong/songfeng0.png',
 						activeImg: '/static/xingkong/songfeng1.png',
 						value: '送风',
+						active: false,
 					},
 					{
 						img: '/static/xingkong/chushi0.png',
 						activeImg: '/static/xingkong/chushi1.png',
 						value: '除湿',
-					}
+						active: false,
+					},
 				],
+				autoModel: {
+					value: '自动',
+					active: false,
+				},
+				activeWindSpeed: {
+					value: '低风'
+				},
 				windSpeedList: [
 					'低风',
 					'中风',
@@ -126,11 +146,77 @@
 				],
 			};
 		},
-		methods: {
-			changeModel(item, index) {
-				this.modelIndex = index
-				console.log(item, index)
+		computed: {
+			nowAir() {
+				let model = this.activeModel.value;
+				let wind = this.activeWindSpeed.value;
+				let res = {
+					model,
+					wind,
+					wendu: '20°'
+				}
+				// console.log('现在模式', res)
+				return res
 			}
+		},
+		methods: {
+			// 开关空调
+			switchAir() {
+				this.isAir = !this.isAir
+				// 关闭模式和风速
+				if (!this.isAir) {
+					this.closeAir()
+				}else {
+					this.initAll()
+				}
+			},
+			// 模式切换
+			changeModel(item, index) {
+				// console.log(this.modelList)
+				// console.log(Object.prototype.toString.call(this.modelList))
+				// console.log(this.modelList.length)
+				for (let i = 0; i < this.modelList.length; i++) {
+					this.modelList[i].active = false
+					if (index === i) {
+						this.modelList[i].active = true
+						this.activeModel = this.modelList[i]
+					}
+				}
+				// this.modelIndex = index
+				// console.log(item, index)
+			},
+			// 自动风
+			changeAutoModel() {
+				this.autoModel.active = !this.autoModel.active
+				this.activeWindSpeed.value = this.autoModel.value
+			},
+			// 初始化模式风速
+			initAll() {
+				this.activeModel=  {
+					img: '/static/xingkong/cool0.png',
+					activeImg: '/static/xingkong/cool1.png',
+					value: '制冷',
+					active: true,
+				}
+				this.activeWindSpeed = {
+					value:'低风'
+				}
+			},
+			// 关闭
+			closeAir() {
+				this.closeModel()
+				this.closeAutoModel()
+			},
+			// 关闭所有模式
+			closeModel() {
+				for (let i = 0; i < this.modelList.length; i++) {
+					this.modelList[i].active = false
+				}
+			},
+			// 关闭所有风速
+			closeAutoModel() {
+				this.autoModel.active = false
+			},
 		}
 	}
 </script>
--- pages/xingkongSon/lamplight/index.vue
+++ pages/xingkongSon/lamplight/index.vue
@@ -4,7 +4,7 @@
 			<!-- <view class="head-box close-bg"> -->
 			<view class="head-box">
 				<image class="bg" src="/static/xingkong/lamplight.png" mode="widthFix"></image>
-			<!-- 亮度盒子 -->
+				<!-- 亮度盒子 -->
 				<view class="lamplight-box">
 					<view class="title">
 						亮度
@@ -22,51 +22,36 @@
 					</view>
 				</view>
 			</view>
-		<!-- 脚部 -->
-		<view class="foot">
-			<view class="foot-item flex-center-center">
-				<view class="img-box">
-				<image class="switch" src="/static/xingkong/switch0.png" mode="widthFix"></image>
+			<!-- 脚部 -->
+			<view class="foot">
+				<view class="foot-item flex-center-center">
+					<view class="img-box">
+						<image class="switch" src="/static/xingkong/switch0.png" mode="widthFix"></image>
+					</view>
+					<view class="text">
+						开/关
+					</view>
 				</view>
-				<view class="text">
-					开/关
+				<view @click="open('model')" class="foot-item active-model flex-center-center">
+					<view class="img-box active-model-img-box">
+						<image class="active-model-img" :src="activeModel.img" mode="widthFix"></image>
+					</view>
+					<view class="text">
+						{{ activeModel.value }}
+					</view>
 				</view>
 			</view>
-			<view @click="open('model')" class="foot-item flex-center-center">
-				<view class="img-box">
-				<image class="yuedu" src="/static/xingkong/yuedu0.png" mode="widthFix"></image>
-				</view>
-				<view class="text">
-				阅读模式
-				</view>
-			</view>
-		</view>
 		</view>
 		<!-- 弹窗 -->
 		<uni-popup ref='model' type="bottom">
 			<view class="popup">
-				<view class="popup-item">
-					<image src="/static/xingkong/yuedu.png" mode="widthFix"></image>
-					<view class="text">
-						阅读模式
+				<view @click="changModel(item,index)" class="popup-item" v-for="(item,index) in modelList" :key='index'>
+					<view class="img-box flex-center-center">
+						<image :src="item.img" mode="widthFix"></image>
+
 					</view>
-				</view>
-				<view class="popup-item">
-					<image src="/static/xingkong/bulb.png" mode="widthFix"></image>
 					<view class="text">
-						明亮模式
-					</view>
-				</view>
-				<view class="popup-item">
-					<image src="/static/xingkong/love.png" mode="widthFix"></image>
-					<view class="text">
-						浪漫模式
-					</view>
-				</view>
-				<view class="popup-item">
-					<image src="/static/xingkong/sleep.png" mode="widthFix"></image>
-					<view class="text">
-						睡眠模式
+						{{ item.value }}
 					</view>
 				</view>
 			</view>
@@ -77,11 +62,37 @@
 <script>
 	import uniPopup from '@/components/uni-popup/uni-popup.vue'
 	export default {
-		components:{
+		components: {
 			uniPopup
 		},
 		data() {
 			return {
+				activeModel: {
+						img: '/static/xingkong/yuedu0.png',
+						value: '阅读模式',
+						active: true,
+					},
+				modelList: [{
+						img: '/static/xingkong/yuedu0.png',
+						value: '阅读模式',
+						active: true,
+					},
+					{
+						img: '/static/xingkong/bulb.png',
+						value: '明亮模式',
+						active: false,
+					},
+					{
+						img: '/static/xingkong/love.png',
+						value: '浪漫模式',
+						active: false,
+					},
+					{
+						img: '/static/xingkong/sleep.png',
+						value: '睡眠模式',
+						active: false,
+					}
+				],
 				lamplightList: [
 					'/static/xingkong/brightness0.png',
 					'/static/xingkong/brightness1.png',
@@ -90,12 +101,28 @@
 				],
 			};
 		},
-		methods:{
+		methods: {
+			// 开关灯
+			switchDeng(){
+				
+			},
 			open(ref) {
 				this.$refs[ref].open()
 			},
 			close(ref) {
 				this.$refs[ref].close()
+			},
+			// 切换模式
+			changModel(item,index) {
+				for (let i = 0; i < this.modelList.length; i++) {
+					this.modelList[i].active = false
+					if(index === i ){
+							this.modelList[i].active = true
+							this.activeModel = this.modelList[i]
+					}
+				}
+				// 关闭弹窗
+				this.close('model')
 			},
 		}
 	}
