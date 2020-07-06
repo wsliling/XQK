541839f1226fbd9a975c0cc47810c7fa5d764b65
Dowling007 <1327716235@qq.com>
2020-07-06 09:18:33 +0800

    星控-空调+ 星控灯光静态页

---
+++ pages/xingkongSon/airConditioner/index.vue
@@ -0,0 +1,140 @@
+<template>
+	<view>
+		<view class="big-box">
+			<!-- 头部状态盒子 -->
+			<!-- <view class="status-box"  :style="{background:`url(${bgImg}) no-repeat center / 100%`}"> -->
+			<view class="status-box">
+				<image class="bg-img" :src="bgImg" mode="widthFix"></image>
+				<view class="mask">
+					<view class="status-show-box">
+						<view class="status-img-box">
+							<image src="/static/xingkong/taiyang2.png" mode="widthFix"></image>
+						</view>
+						<view class="status">
+							<view class="model">
+								<view class="left">
+									制冷模式
+								</view>
+								<view class="center">
+									|
+								</view>
+								<view class="right">
+									自动风
+								</view>
+							</view>
+							<view class="describe">
+								20°
+							</view>
+						</view>
+					</view>
+
+				</view>
+
+			</view>
+			<!-- 控制内容盒子 -->
+			<view class="content-box">
+				<!-- 开关盒子 -->
+				<view class="switch-box flex-center-center">
+					<view class="switch flex-center-center">
+						<image src="/static/xingkong/switch0.png" mode="widthFix"></image>
+					</view>
+					<view class="switch-text">
+						开/关
+					</view>
+				</view>
+				<!-- 模式盒子 -->
+				<view class="model-box flex-center-between">
+					<view class="model-item" :class="[modelIndex === index ? 'active' : '',index === 0 ? 'w42' : '',index === 3 ? 'w39' : '']" v-for="(item,index) in modeList" :key='index'
+					 @click="changeModel(item,index)">
+						<image v-if="modelIndex !== index || modelIndex === -1" :src="item.img" mode="widthFix"></image>
+						<image v-else :src="item.activeImg" mode="widthFix"></image>
+						<view class="model-value">
+							{{ item.value }}
+						</view>
+					</view>
+				</view>
+				<!-- 风速盒子 -->
+				<view class="wind-speed-box">
+					<view class="title">
+						风速
+					</view>
+					<view class="content">
+						<view class="left">
+							<view class="wind-speed-grade">
+								<view class="progress-bar">
+									<view class="active-progress-bar">
+									</view>
+								</view>
+								<view class="wind-speed-describe">
+									<view class="wind-speed-describe-item" v-for="(item,index) in windSpeedList" :key='index'>
+										{{ item }}
+									</view>
+								</view>
+							</view>
+						</view>
+						<view class="right">
+							<view class="huan flex-center-center">
+								<view class="huan-yuan">
+									
+								</view>
+							</view>
+							<view class="auto-model">
+								自动
+							</view>
+						</view>
+					</view>
+					
+
+				</view>
+			</view>
+
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				bgImg: '/static/xingkong/bg1.png',
+				modelIndex: -1,
+				modeList: [{
+						img: '/static/xingkong/cool0.png',
+						activeImg: '/static/xingkong/cool1.png',
+						value: '制冷',
+					},
+					{
+						img: '/static/xingkong/zhire0.png',
+						activeImg: '/static/xingkong/zhire1.png',
+						value: '制热',
+					},
+					{
+						img: '/static/xingkong/songfeng0.png',
+						activeImg: '/static/xingkong/songfeng1.png',
+						value: '送风',
+					},
+					{
+						img: '/static/xingkong/chushi0.png',
+						activeImg: '/static/xingkong/chushi1.png',
+						value: '除湿',
+					}
+				],
+				windSpeedList: [
+					'低风',
+					'中风',
+					'高风'
+				],
+			};
+		},
+		methods: {
+			changeModel(item, index) {
+				this.modelIndex = index
+				console.log(item, index)
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	@import './style';
+</style>
---
+++ pages/xingkongSon/lamplight/index.vue
@@ -0,0 +1,107 @@
+<template>
+	<view>
+		<view class="big-box">
+			<!-- <view class="head-box close-bg"> -->
+			<view class="head-box">
+				<image class="bg" src="/static/xingkong/lamplight.png" mode="widthFix"></image>
+			<!-- 亮度盒子 -->
+				<view class="lamplight-box">
+					<view class="title">
+						亮度
+					</view>
+					<view class="lamplight-grade">
+						<view class="progress-bar">
+							<view class="active-progress-bar">
+							</view>
+						</view>
+						<view class="lamplight-describe">
+							<view class="lamplight-describe-item" v-for="(item,index) in lamplightList" :key='index'>
+								<image :src="item" mode="widthFix"></image>
+							</view>
+						</view>
+					</view>
+				</view>
+			</view>
+		<!-- 脚部 -->
+		<view class="foot">
+			<view class="foot-item flex-center-center">
+				<view class="img-box">
+				<image class="switch" src="/static/xingkong/switch0.png" mode="widthFix"></image>
+				</view>
+				<view class="text">
+					开/关
+				</view>
+			</view>
+			<view @click="open('model')" class="foot-item flex-center-center">
+				<view class="img-box">
+				<image class="yuedu" src="/static/xingkong/yuedu0.png" mode="widthFix"></image>
+				</view>
+				<view class="text">
+				阅读模式
+				</view>
+			</view>
+		</view>
+		</view>
+		<!-- 弹窗 -->
+		<uni-popup ref='model' type="bottom">
+			<view class="popup">
+				<view class="popup-item">
+					<image src="/static/xingkong/yuedu.png" mode="widthFix"></image>
+					<view class="text">
+						阅读模式
+					</view>
+				</view>
+				<view class="popup-item">
+					<image src="/static/xingkong/bulb.png" mode="widthFix"></image>
+					<view class="text">
+						明亮模式
+					</view>
+				</view>
+				<view class="popup-item">
+					<image src="/static/xingkong/love.png" mode="widthFix"></image>
+					<view class="text">
+						浪漫模式
+					</view>
+				</view>
+				<view class="popup-item">
+					<image src="/static/xingkong/sleep.png" mode="widthFix"></image>
+					<view class="text">
+						睡眠模式
+					</view>
+				</view>
+			</view>
+		</uni-popup>
+	</view>
+</template>
+
+<script>
+	import uniPopup from '@/components/uni-popup/uni-popup.vue'
+	export default {
+		components:{
+			uniPopup
+		},
+		data() {
+			return {
+				lamplightList: [
+					'/static/xingkong/brightness0.png',
+					'/static/xingkong/brightness1.png',
+					'/static/xingkong/brightness2.png',
+					'/static/xingkong/brightness3.png',
+				],
+			};
+		},
+		methods:{
+			open(ref) {
+				this.$refs[ref].open()
+			},
+			close(ref) {
+				this.$refs[ref].close()
+			},
+		}
+	}
+</script>
+
+
+<style lang="scss" scoped>
+	@import './style';
+</style>
